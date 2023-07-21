b05e617a0dcf7cc4d83049c1a6aa383c8e9fd343
Jorge Matos <jisapjisap@gmail.com>
2023-07-21 16:28:15 +0200

    Search

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -9,4 +9,5 @@ abstract class MoviesDatasource { // Aquí solo definimos como van a lucir los d
   Future<List<Movie>> getUpcoming({int page = 1});
   Future<List<Movie>> getTopRated({int page = 1});
   Future<Movie> getMovieById( String id );
+  Future<List<Movie>> searchMovies( String query );
 }
